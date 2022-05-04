ac85a0a208a2fc6428a6755814a49c70978c93b9
HusseinGhulam <82550066+HusseinGhulam@users.noreply.github.com>
2022-05-05 00:26:18 +0300

    Add files via upload

---
+++ writeups/wu-js.js
@@ -0,0 +1,13 @@
+$("#nav-home-tab").click(function(){
+    $(".thm").show()
+    $(".pico").show()
+})
+
+$("#thm-btn").click(function(){
+    $(".pico").hide()
+    $(".thm").show()
+})
+$("#pico-btn").click(function(){
+    $(".thm").hide()
+    $(".pico").show()
+})
